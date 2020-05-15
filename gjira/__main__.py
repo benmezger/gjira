3fb0dac96789c309f949e1225e3ea2d62f394444
Ben Mezger <me@benmezger.nl>
2020-05-15 19:31:11 -0300

    Refactor: Move file to a package

---
+++ gjira/__main__.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import sys
+from jira import JIRA
+from .gjira import (
+    get_jira_from_env,
+    get_branch_name,
+    get_issue,
+    get_issue_parent,
+    update_commit_message,
+    DEFAULT_MSG,
+)
+
+import argparse
+
+
+def arg_parser(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filenames", nargs="+")
+    parser.add_argument("--format", default=DEFAULT_MSG)
+    parser.add_argument("--board")
+    return parser.parse_args(argv)
+
+
+def get_branch_id():
+    branch = get_branch_name().split("/")
+
+    if len(branch) == 1:
+        raise ValueError("Bad branch name. Expected format of <id>/<txt>")
+
+    return branch[0]
+
+
+def main(argv=None):
+    args = arg_parser(argv)
+    options = get_jira_from_env()
+    jira = JIRA(**options)
+
+    task_id = get_branch_id()
+
+    # branch_issue = get_issue(jira, ticket_id)
+    branch_issue = get_issue(jira, task_id)
+    branch_story = get_issue_parent(branch_issue)
+
+    fmt = (args.format or DEFAULT_MSG).format(branch_issue, branch_story)
+
+    update_commit_message(args.filenames[0], fmt)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
--- gjira/gjira.py
+++ gjira/gjira.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
 from jira import JIRA
-import os, sys
+import os
 import pathlib
-import argparse
 import subprocess
 
 
@@ -14,14 +13,6 @@ def get_branch_name():
     return subprocess.check_output(
         ("git", "rev-parse", "--abbrev-ref", "HEAD",),
     ).decode("UTF-8")
-
-
-def arg_parser(argv):
-    parser = argparse.ArgumentParser()
-    parser.add_argument("filenames", nargs="+")
-    parser.add_argument("--format", default=DEFAULT_MSG)
-    parser.add_argument("--board")
-    return parser.parse_args(argv)
 
 
 def get_jira_from_env() -> dict:
@@ -51,29 +42,3 @@ def update_commit_message(filename, fmt):
             fd.write(f"\n{fmt}")
             return
         fd.write(f"\n\n{fmt}")
-
-
-def main(argv=None):
-    args = arg_parser(argv)
-
-    options = get_jira_from_env()
-    jira = JIRA(**options)
-
-    branch = get_branch_name().split("/")
-
-    if len(branch) == 1:
-        raise ValueError("Bad branch name. Expected format of <id>/<txt>")
-
-    ticket_id = branch[0]
-
-    # branch_issue = get_issue(jira, ticket_id)
-    branch_issue = get_issue(jira, "SKYR-1990")
-    branch_story = get_issue_parent(branch_issue)
-
-    fmt = (args.format or DEFAULT_MSG).format(branch_issue, branch_story)
-
-    update_commit_message(args.filenames[0], fmt)
-
-
-if __name__ == "__main__":
-    sys.exit(main())
