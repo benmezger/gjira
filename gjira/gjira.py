7ab188504ade77474e5571ab10215e57922b932a
Ben Mezger <me@benmezger.nl>
2020-05-23 23:33:28 -0300

    Fix: Don't slice lines before writing to file

--- gjira/gjira.py
+++ gjira/gjira.py
@@ -98,7 +98,7 @@ def update_commit_message(filename: str, content: str) -> list:
             content = f"\n{content}\n"
 
         # add fmt to the corresponding position and read any unread line
-        lines = lines[:pos] + [content] + fd.readlines()
+        lines = lines + [content] + fd.readlines()
 
         # Write lines back to file
         fd.seek(0)
