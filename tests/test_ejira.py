8f303e3878ceff6dfbb72b369ab13a38d36746de
Ben Mezger <me@benmezger.nl>
2020-05-24 00:51:49 -0300

    Feat: Update test coverage on gjira module

--- tests/test_ejira.py
+++ tests/test_ejira.py
@@ -20,7 +20,8 @@ def test_issue_attr(jira_issue, jira_attributes):
 
 
 def test_issue_attr_with_invalid_missing_attrs(jira_issue, jira_attributes):
-    delattr(getattr(jira_issue, "fields"), "votes")
+    mocker.patch.object("jira.Issue", "fields", None)
+    # delattr(getattr(jira_issue, "fields"), "votes")
 
     result = {}
     for attr in jira_attributes:
@@ -173,3 +174,49 @@ def test_update_commit_msg_with_empty_text(mocker):
     assert f"\nJira information:\n{fmt}\n\n" == write.write.call_args_list[0].args[0]
     for (line, call) in zip(file_text.split("\n")[1:], write.write.call_args_list[2:]):
         assert line.strip() in call.args[0].strip("\n")
+
+
+def test_get_issue_raises_exception_and_return_empty_dict(mocker, jira_connection):
+    requests_get_mock = mocker.patch("requests.sessions.Session.get")
+    requests_get_mock.return_value.status_code = 404
+    requests_get_mock.return_value.text = {}
+
+    result = gjira.get_issue(jira_connection, "JIRA_ID", ("a, b"))
+    assert result == {}
+
+
+def test_is_jira_in_file(mocker):
+    file_text = f"""
+    # A properly formed Git commit subject line should always be able to complete
+    # the following sentence:
+    #     * If applied, this commit <will your subject line here>
+    #
+    # [Add/Fix/Remove/Update/Refactor/Document/Style]: [issue #id] [summary]
+
+    {gjira.GJIRA_START_TEXT}\n:
+    Issue: ..
+    """
+
+    open_mock = mocker.patch("builtins.open", mocker.mock_open(read_data=file_text))
+
+    result = gjira.is_gjira_in_file("file_text_path")
+    open_mock.assert_called_once_with("file_text_path")
+    assert result == True
+
+
+def test_is_jira_in_file(mocker):
+    file_text = f"""
+    # A properly formed Git commit subject line should always be able to complete
+    # the following sentence:
+    #     * If applied, this commit <will your subject line here>
+    #
+    # [Add/Fix/Remove/Update/Refactor/Document/Style]: [issue #id] [summary]
+
+    Issue: ..
+    """
+
+    open_mock = mocker.patch("builtins.open", mocker.mock_open(read_data=file_text))
+
+    result = gjira.is_gjira_in_file("file_text_path")
+    open_mock.assert_called_once_with("file_text_path")
+    assert result == False
