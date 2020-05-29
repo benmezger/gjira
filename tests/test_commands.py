981f9c2b1908625ccad93b2b938a58da90fad444
Ben Mezger <me@benmezger.nl>
2020-05-29 00:31:44 -0300

    Feat: Add commands test

---
+++ tests/test_commands.py
@@ -0,0 +1,59 @@
+from click.testing import CliRunner
+import pytest
+
+from gjira.commands import cmd_update_commit_msg, cmd_validate_branch_name
+
+
+def test_cmd_update_commit_msg(mocker, cli, git_branch, jira_connection, jira_issue):
+    git_branch("SKYR-123")
+    with cli.isolated_filesystem():
+        with open(".commit.template", "w") as commitf:
+            commitf.write("Jira issue: {{key}}\nJira story {{parent__key}}")
+
+        with open("COMMIT_MSG", "w") as msgf:
+            msgf.write("Feat: A commit message")
+
+        result = cli.invoke(
+            cmd_update_commit_msg,
+            ["--regex", "SKYR-\d+", "--board", "boardname", "COMMIT_MSG"],
+        )
+        assert result.exit_code == 0
+        assert result.output == ""
+        assert result.exception == None
+
+
+def test_cmd_validate_branch_name_with_branch_specified(cli, git_branch):
+    result = cli.invoke(
+        cmd_validate_branch_name,
+        ["--regex", "JIRA-\d+_[a-z]+(-[a-z]+)*$", "--branch", "JIRA-123_hello-world"],
+    )
+    assert result.exit_code == 0
+    assert result.output == ""
+    assert result.exception == None
+
+
+def test_cmd_validate_branch_name_with_no_branch_specified(cli, git_branch):
+    git_branch("JIRA-123_hello-world")
+    result = cli.invoke(
+        cmd_validate_branch_name, ["--regex", "JIRA-\d+_[a-z]+(-[a-z]+)*$"]
+    )
+    assert result.exit_code == 0
+    assert result.output == ""
+    assert result.exception == None
+
+
+def test_cmd_validate_branch_name_with_invalid_name(cli):
+    result = cli.invoke(
+        cmd_validate_branch_name,
+        [
+            "--regex",
+            "JIRA-\d+_[a-z]+(-[a-z]+)*$",
+            "--branch",
+            "JIRA-1234_hello_world-123",
+        ],
+    )
+    assert result.exit_code == 1
+    assert (
+        result.output
+        == "Branch name requires the format of 'JIRA-\\d+_[a-z]+(-[a-z]+)*$'. Aborting.\n"
+    )
