9d9e5ea16bb2d745dbca0888fa197ed511b933b5
Ben Mezger <me@benmezger.nl>
2020-06-07 00:25:14 -0300

    Feat: Ignore flag for MERGE_MSG and SQUASH_MSG by default

--- gjira/commands.py
+++ gjira/commands.py
@@ -1,3 +1,4 @@
+from typing import Iterable, Union
 import pathlib
 import sys
 import click
@@ -19,13 +20,32 @@ from .git import get_branch_id, get_branch_name, validate_branch_name
 @click.option("--board", "-b", required=True, type=str)
 @click.option("--regex", "-r", required=True, type=str)
 @click.option(
+    "--ignore-files",
+    "-i",
+    default=",".join(("MERGE_MSG", "SQUASH_MSG",)),
+    show_default=True,
+)
+@click.option(
     "--template",
     "-t",
     type=click.Path(exists=True, writable=True, readable=True, resolve_path=True),
     default=str(pathlib.Path(".").joinpath(".commit.template")),
 )
 @click.argument("filename")
-def cmd_update_commit_msg(filename: str, board: str, regex: str, template: str):
+def cmd_update_commit_msg(
+    filename: str,
+    board: str,
+    regex: str,
+    template: str,
+    ignore_files: Union[Iterable, str],
+):
+    if isinstance(ignore_files, str):
+        ignore_files = ignore_files.split(",")
+
+    if filename in ignore_files:
+        print(f"File '{filename}'. Skipping.")
+        sys.exit(0)
+
     if is_gjira_in_file(filename):
         print("Duplicated. Skipping")
         sys.exit(0)
